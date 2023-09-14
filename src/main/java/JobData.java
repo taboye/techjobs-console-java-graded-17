1e41d2ca02f3019c120c9c4a378635594c5334a6
Tarekegne <tarinium@yahoo.com>
2023-09-14 15:37:25 -0400

    PrintJob code added

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -95,7 +95,16 @@ public class JobData {
         loadData();
 
         // TODO - implement this method
-        return null;
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+        for (HashMap<String, String> row : allJobs) {
+
+            String aValue = row.get(value);
+
+            if (aValue.contains(value)) {
+                jobs.add(row);
+            }
+        }
+        return jobs;
     }
 
     /**
--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -10,7 +10,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -112,14 +112,27 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        if (!someJobs.isEmpty()) {
+            for (HashMap<String, String> someTask : someJobs) {
+                System.out.println("\n*****");
+                for (Map.Entry<String, String> job : someTask.entrySet()) {
+                    System.out.println(job.getKey() + ": " + job.getValue());
+                }
+                System.out.println("*****");
+            }
+            }else{
+                System.out.print("No Results");
+            }
+            // System.out.println("printJobs is not implemented yet");
+        }
     }
-}
+
+
+
